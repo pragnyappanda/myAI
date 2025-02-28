564ef4629e9fd1784ff7b3c17398fca7e62af036
pragnyappanda <5788pragnya@gmail.com>
2025-02-28 18:12:27 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${MunchMate}, ${Pragnya}'s AI assistant. Tell me what you’re craving, what ingredients you have, or your dietary preferences, and I’ll whip up a delicious, easy-to-make recipe for you! `;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
