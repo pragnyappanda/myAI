2c53af03d745380857fedb995f59cb648a24cf64
pragnyappanda <5788pragnya@gmail.com>
2025-02-28 19:34:57 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[Meal Prep Buddy]`;
+export const CHAT_HEADER: string = `MunchMate`;
 export const MESSAGE_PLACEHOLDER: string = `[Type your message here]`;
 export const FOOTER_MESSAGE: string = `[Custom AI by Panda]`;
 export const CLEAR_BUTTON_TEXT: string = `[Edit]`;
-export const PAGE_TITLE: string = `[Indian Meal buddy]`;
+export const PAGE_TITLE: string = `Meal prep buddy`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
